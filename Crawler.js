d95a74134588ae83c683a076b4c1e3a782bf136b
zhangweihuang77 <zhangweihuang77@sina.com>
2017-02-07 11:05:33 +0800

    多了个mongodb

--- Crawler.js
+++ Crawler.js
@@ -6,10 +6,23 @@ var request = require("request");
 var cheerio = require("cheerio");
 var xlsx = require("node-xlsx");
 var sd = require('silly-datetime');
+var MongoClient = require('mongodb').MongoClient
+    , assert = require('assert');
+var url = 'mongodb://localhost:27017/node';
 var startTime = sd.format(new Date(), 'YYYY-MM-DD HH:mm');
-var movie = 0, s = 0, num = 0, ttt = 0, year = 1988;
+var movie = 0, s = 0, num = 0, ttt = 0, year = 1950;
 var data = [];
 var obj = [];
+var node = [];
+
+var insertDocuments = function(db , node) {
+    // Get the documents collection
+    var collection = db.collection('pachong');
+    // Insert some documents
+    collection.insertMany(node, function(err, result) {
+        node.splice(0,node.length);
+    });
+}
 
 function classA(body) {
     var $ = cheerio.load(body);
@@ -25,29 +38,29 @@ function classA(body) {
                 request(nbgAry[ttt], function (error, response, body) {
                     if (!error && response.statusCode == 200) {
                         var $ = cheerio.load(body);
-                        var text = $("#info").text();
-                        var regionItem = (text.indexOf("地区:") + 4);
-                        var time = $("span[property='v:initialReleaseDate']").eq(0).text();
+                        var time = $("span[property='v:initialReleaseDate']").text();
                         var film = $("span[property='v:itemreviewed']").text();
-                        var director = $("a[rel='v:directedBy']").text();
                         var rating_num = $(".rating_num").text();
-                        var j = 0;
-                        while (text.slice(regionItem, regionItem + j).indexOf("\n") < 0) {
-                            j++;
-                            var region = text.slice(regionItem, regionItem + j - 1);
-                        }
-                        data[movie] = [movie + 1, film, director, region, time, rating_num];
+                        data[movie] = [movie, film, time, rating_num];
+                        node.push({film:film,time:time,rating_num:rating_num});
                         console.log(data[movie]);
                         movie += 1;
                         ttt += 1;
                         setTimeout(function () {
                             classB();
-                        }, 800);
+                        }, 500);
                     }
                 })
             } else {
                 ttt = 0;
-                req("https://movie.douban.com/tag/" + year + "?start=" + (num += 20) + "&type=T");
+                MongoClient.connect(url, function(err, db) {
+                    // assert.equal(null, err);
+                    insertDocuments(db ,node);
+                    db.close();
+                });
+                setTimeout(function () {
+                    req("https://movie.douban.com/tag/" + year + "?start=" + (num += 20) + "&type=T");
+                }, 500);
             }
         }
 
